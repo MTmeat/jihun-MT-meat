ac0298a8e0b8f0f46345da9e48699539004153d0
cjh5414 <cjh5414@gmail.com>
2017-09-25 15:25:47 +0900

    Add FT for select meat count

--- jihunMTmeat/functional_tests/mainpage_tests.py
+++ jihunMTmeat/functional_tests/mainpage_tests.py
@@ -1,7 +1,30 @@
+import time
+
 from fixture_tests import browser, BASE_URL
 
+from selenium.webdriver.common.keys import Keys
 
-def test__main_page(browser):
+
+def test_main_page(browser):
     browser.get(BASE_URL)
 
     assert BASE_URL + "/" == browser.current_url
+
+
+def test__select_meat_count(browser):
+    browser.get(BASE_URL)
+
+    browser.find_element_by_xpath("//input[@name='삼겹']").clear()
+    browser.find_element_by_xpath("//input[@name='삼겹']").send_keys('5')
+    browser.find_element_by_xpath("//input[@name='목살']").clear()
+    browser.find_element_by_xpath("//input[@name='목살']").send_keys('4')
+
+    browser.find_element_by_xpath("//input[@type='submit']").send_keys(Keys.ENTER)
+
+    assert browser.current_url == BASE_URL + '/ordermeats/new/'
+    assert browser.find_element_by_xpath("//input[@name='삼겹']").get_attribute('value') == '5'
+    assert browser.find_element_by_xpath("//input[@name='목살']").get_attribute('value') == '4'
+
+
+
+
